4cfd815e6b91a04ca2c359ad679a85207556fc90
[zanzers] <[nowellsaavedra999@gmail.com]>
2024-11-17 10:30:59 +0800

    books update

--- app.py
+++ app.py
@@ -4,9 +4,64 @@ from http import HTTPStatus
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_word():
-    return "Hello Word"
+books = [
+    {
+        'id': 1,
+        'Title': 'To Kill a Mockingbird',
+        'author': 'Harper Lee',
+        'year': 1960,
+    },
+    {
+        'id': 2,
+        'Title': '1984',
+        'author': 'George Orwell',
+        'year': 1949,
+    },
+    {
+        'id': 3,
+        'Title': 'Pride and Prejudice',
+        'author': 'Jane Austen',
+        'year': 1813,
+    },
+    {
+        'id': 4,
+        'Title': 'The Great Gatsby',
+        'author': 'F. Scott Fitzgerald',
+        'year': 1925,
+    },
+    {
+        'id': 5,
+        'Title': 'Moby-Dick',
+        'author': 'Herman Melville',
+        'year': 1851,
+    }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# @app.route('/')
+# def hello_word():
+#     return "Hello Word"
 
 
 
